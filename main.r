9b0ffd0644fc9ed6c7370623845fd1eb3a6de456
adamhamdan250 <adam_hamdan@brown.edu>
2024-04-16 09:52:33 -0400

    create main.r script, runs all code

---
+++ main.r
@@ -0,0 +1,10 @@
+# Run all code for the project
+
+# Get raw network data and clean it
+source("./code/get_data.r")
+
+## Make network plots
+source("./code/make_plots.r")
+
+## Make summary statistics table
+source("./code/make_table.r")
