7535ee75f51a9f23dc7e65a4993ed7ee80fca9e4
adamhamdan250 <adam_hamdan@brown.edu>
2024-04-16 10:11:00 -0400

    added test that checks if all nodes in graph_connected are connected

--- tests/test-network.r
+++ tests/test-network.r
@@ -1,3 +1,9 @@
 test_that("Test # nodes in graph_large <= # nodes in graph", {
   expect_true(g_original_num_nodes >= g_num_nodes)
 })
+
+test_that("Test that all nodes in graph_connected have 1 or more connections", {
+  g_degree_connected <- degree(graph_connected)
+  g_degree_connected_min <- min(g_degree_connected)
+  expect_true(g_degree_connected_min >= 1)
+})
