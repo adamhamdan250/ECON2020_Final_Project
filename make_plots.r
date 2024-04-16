a55528a7706bfabd1427bc5538e17c2c0ecfc5e5
adamhamdan250 <adam_hamdan@brown.edu>
2024-04-16 09:36:05 -0400

    generate all plots, exported to output folder as pdfs

--- make_plots.r
+++ make_plots.r
@@ -30,6 +30,7 @@ dev.off()
 # Open PDF device
 pdf("output/plot_connected_network.pdf")
 
+# Generate plot
 fig_network_connected <- plot(graph_connected, 
      vertex.label=NA,
      vertex.size = 5,
@@ -39,7 +40,15 @@ fig_network_connected <- plot(graph_connected,
      main = "All Connected Components"
 )
 
+# Close PDF device
+dev.off()
+
 # PLOT 3: Largest connected component
+
+# Open PDF device
+pdf("output/plot_largest_network.pdf")
+
+# Generate plot
 fig_network_largest <- plot(graph_large,
      vertex.label=NA,
      vertex.size = 5,
@@ -48,6 +57,9 @@ fig_network_largest <- plot(graph_large,
      edge.width = 1,
      main = "Largest Connected Component"
 )
+
+# Close PDF device
+dev.off()
 
 # PLOT 4: Most connected node
 
@@ -65,7 +77,10 @@ vertex_colors <- rep("#cccccc", vcount(graph_large))  # Default color for all ve
 vertex_colors[g_most_connected] <- "#2b2ba2"  # Color for the most connected node
 vertex_colors[g_most_connected_neighbours] <- "#7777ff"  # Color for the neighbors
 
-# Plot netork, highlighting most connected node and its neigbhbours
+# Open PDF device
+pdf("output/plot_most_connected_node.pdf")
+
+# Generate plot
 fig_network_most_connected <- plot(
      graph_large,
      vertex.label = NA,
@@ -76,5 +91,6 @@ fig_network_most_connected <- plot(
      main = "Most Connected Node"
 )
 
-# Export plots to "/output" folder
-ggsave("output/original_network.pdf", plot = fig_network)
+# Close PDF device
+dev.off()
+
